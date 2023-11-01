29577a51ca71493823797bf881bb798ad94523c1
MN <114230535+E-E-N@users.noreply.github.com>
2023-11-02 05:57:27 +0800

    Update book.go

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -1,7 +1,7 @@
 package models
 
 import (
-	"github.com/akhil/gp bpplstpre/pkg/config"
+	"github.com/E-E-N/Go-BookStore/pkg/config"
 	"github.com/jinzhu/gorm"
 )
 
@@ -9,7 +9,7 @@ var db *gorm.DB
 
 type Book struct {
 	gorm.Model
-	Name        string `gorm:""json:"name"`
+	Name        string `gorm:"" json:"name"`
 	Author      string `json:"author"`
 	Publication string `json:"publication"`
 }
@@ -18,4 +18,28 @@ func init() {
 	config.Connect()
 	db = config.GetDB()
 	db.AutoMigrate(&Book{})
+}
+
+func (b *Book) CreateBook() *Book {
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book {
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func GetBookById(Id int64) (*Book, *gorm.DB) {
+	var getBook Book
+	db := db.Where("ID=?", Id).Find(&getBook)
+	return &getBook, db
+}
+
+func DeleteBook(ID int64) Book {
+	var book Book
+	db.Where("ID=?", ID).Delete(book)
+	return book
 }
